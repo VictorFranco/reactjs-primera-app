53679ecee30b7cb5bcd5c6a3927dad0a701dd5cc
Victor Franco <vfrancohdez1@hotmail.com>
2021-03-11 00:44:44 -0600

    Los datos fueron organizados con css

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,17 @@
 import React,{Component} from 'react';
+import './Task.css';
 
 class Task extends Component {
   render() {
       const {task}=this.props;
-    return <div>
-        <div>
-        {task.title} <br/> {task.description}
-        </div>
-        <div>
-        <input type="checkbox"></input>
-        <div>X</div>
-        </div>
+    return <div className="Task">
+            <div>
+                {task.title} <br/> {task.description}
+            </div>
+            <div className="group2">
+                <input type="checkbox"></input>
+                <div>X</div>
+            </div>
         </div>;
   }
 }
