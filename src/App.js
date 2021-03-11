3b3b8645d20a454d0b19eaba471a4ed6289e42e6
Victor Franco <vfrancohdez1@hotmail.com>
2021-03-11 09:31:31 -0600

    Funcionalidad agragada al formulario

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
   state={
     tasks:tasks
   }
+  addTask=(title,description)=>{
+    const newTask={
+      title:title,
+      description:description,
+      id:this.state.tasks.length
+    }
+    this.setState({
+      tasks:[...this.state.tasks,newTask]
+    })
+  }
   render() {
     return <div className="App">
       <div>
         <div className="Card">
-          <TaskForm/>
+          <TaskForm addTask={this.addTask}/>
         </div>
         <div className="Card">
           <Tasks tasks={this.state.tasks}/>
